6625aa7765bb6576900347a80f5f97162970ede4
Ting-Hao, Liu <adam.life1115@gmail.com>
2026-09-06 19:40:15 +0800

    Add: clone-graph - submission-13

---
+++ Data Structures & Algorithms/clone-graph/submission-13.py
@@ -0,0 +1,32 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        # Adjacency List
+        # T: O(V + E)
+        # S: O(V)
+        if not node:
+            return None
+
+        oldToNew = {}
+
+        def dfs(currNode: Optional['Node']) -> Optional['Node']:
+            if currNode in oldToNew:
+                return oldToNew[currNode]
+
+            copyNode = Node(currNode.val)
+            oldToNew[currNode] = copyNode
+
+            for nei in currNode.neighbors:
+                copyNode.neighbors.append(dfs(nei))
+            
+            return copyNode
+
+        return dfs(node)
+        
